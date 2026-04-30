9ef749d9e954942688f5cc85df4063d42bfcf1f1
edomiyas888 <edomias888@gmail.com>
2026-04-30 13:26:35 -0700

    init commit

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,5 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: '/yd-car-rental/',
+  base: '/yd-car-rental/', // <--- THIS MUST MATCH YOUR GITHUB REPO NAME EXACTLY})
 })
